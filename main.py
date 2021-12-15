63b375a59a505d213eb94318323e6e203acbcd58
Iepichnieyiighr <95309916+MrForskami@users.noreply.github.com>
2021-12-15 17:35:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+set4 = set() 
+set2 = {0}
+
+set1 = "WED" 
+set4.add("WED") 
+set2.add("SD")
+
+print(set1 in set4) 
+print(set1 in set2)
+
+set3 = "setyou"
+set4.add("setyou")
+set2.add("set")
+
+print (set3 in set4) 
+print (set3 in set2)
